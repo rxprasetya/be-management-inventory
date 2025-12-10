98c68ace7737fb3e7328acf1b201b09cd55125e9
rxprasetya <2118027@scholar.itn.ac.id>
2025-12-10 21:07:45 +0700

    added action log history

--- src/logger/index.js
+++ src/logger/index.js
@@ -2,22 +2,34 @@ import winston from "winston"
 import DailyRotateFile from "winston-daily-rotate-file"
 import { join } from "path"
 
-const transportInfo = new DailyRotateFile({
-    filename: join(process.cwd(), "logs", "%DATE%-app.log"),
-    datePattern: "YYYY-MM-DD",
-    zippedArchive: true,
-    maxSize: "10m",
-    maxFiles: "14d",
-})
+const transports = [
+    new winston.transports.Console({
+        format: winston.format.simple(),
+    }),
+]
 
-const transportError = new DailyRotateFile({
-    level: "error",
-    filename: join(process.cwd(), "logs", "%DATE%-app-errors.log"),
-    datePattern: "YYYY-MM-DD",
-    zippedArchive: true,
-    maxSize: "10m",
-    maxFiles: "14d",
-})
+if (process.env.NODE_ENV !== "production") {
+    transports.push(
+        new DailyRotateFile({
+            filename: join(process.cwd(), "logs", "%DATE%-app.log"),
+            datePattern: "YYYY-MM-DD",
+            zippedArchive: true,
+            maxSize: "10m",
+            maxFiles: "14d",
+        })
+    )
+
+    transports.push(
+        new DailyRotateFile({
+            level: "error",
+            filename: join(process.cwd(), "logs", "%DATE%-app-errors.log"),
+            datePattern: "YYYY-MM-DD",
+            zippedArchive: true,
+            maxSize: "10m",
+            maxFiles: "14d",
+        })
+    )
+}
 
 const logger = winston.createLogger({
     level: "info",
@@ -25,14 +37,7 @@ const logger = winston.createLogger({
         winston.format.timestamp(),
         winston.format.json()
     ),
-    transports: [
-        transportInfo,
-        transportError,
-        process.env.NODE_ENV !== "production" &&
-        new winston.transports.Console({
-            format: winston.format.simple()
-        })
-    ]
+    transports,
 })
 
 export default logger
